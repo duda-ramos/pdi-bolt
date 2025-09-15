d03482224ef684c1848ff56b5430f9b218d638f3
duda-ramos <mariaeduarda@deadesign.com.br>
2025-09-15 17:32:07 -0300

    Updated Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../components/Layout/Header';
 import { useAuth } from '../contexts/AuthContext';
 import { profileService } from '../services/supabase/profiles';
